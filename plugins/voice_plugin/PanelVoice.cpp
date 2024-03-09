ce154bf6d3be2fd2fcb204163438f8897137fbb7
Jung Who Nam <jungwhonam@gmail.com>
2024-03-09 16:18:29 -0600

    Save rgb and depth images

--- plugins/voice_plugin/PanelVoice.cpp
+++ plugins/voice_plugin/PanelVoice.cpp
@@ -3,7 +3,11 @@
 #include "app/MainWindow.h"
 #include "app/widgets/GenerateImGuiWidgets.h"
 
+#include "sg/Frame.h"
+#include "sg/fb/FrameBuffer.h"
+
 #include "imgui.h"
+#include "stb_image_write.h"
 
 namespace ospray {
   namespace voice_plugin {
@@ -11,7 +15,12 @@ namespace ospray {
   PanelVoice::PanelVoice(std::shared_ptr<StudioContext> _context, std::string _panelName)
       : Panel(_panelName.c_str(), _context)
       , panelName(_panelName)
-  {}
+  {
+    // enable floatFormat for OSP_FB_DEPTH channel
+    auto &fb = _context->frame->childAs<sg::FrameBuffer>("framebuffer");
+    fb["floatFormat"] = true;
+    fb.commit();
+  }
 
   void PanelVoice::buildUI(void *ImGuiCtx)
   {
@@ -23,28 +32,84 @@ namespace ospray {
     ImGui::SetCurrentContext((ImGuiContext *)ImGuiCtx);
     ImGui::OpenPopup(panelName.c_str());
 
-    if (ImGui::BeginPopupModal(
-            panelName.c_str(), nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
-      ImGui::Text("%s", "Hello from the voice plugin!");
-      ImGui::Separator();
+    if (!ImGui::BeginPopupModal(panelName.c_str(), nullptr, ImGuiWindowFlags_None)) return;
 
-      ImGui::Text("%s", "Current application state");
-      ImGui::Text("Frame: %p", (void *) context->frame.get());
-      auto pMW = (MainWindow*)context->getMainWindow();
-      if (pMW)
-        ImGui::Text("Arcball Camera: %p", (void *) pMW->arcballCamera.get());
-      ImGui::Text("Current scenegraph:");
-      context->frame->traverse<sg::GenerateImGuiWidgets>(
-          sg::TreeState::ROOTOPEN);
+    if (ImGui::Button("Capture (RGB)")) {
+      auto &fb = context->frame->childAs<sg::FrameBuffer>("framebuffer");
+      auto size = fb.child("size").valueAs<vec2i>();
+      const vec4f *mapped = static_cast<const vec4f *>(fb.map(OSP_FB_COLOR));
+      size_t npix = size.x * size.y;
+      vec4uc *newfb = (vec4uc *)malloc(npix * sizeof(vec4uc));
+      
+      if (newfb) {
+        // float to char
+        for (size_t i = 0; i < npix; i++) {
+          auto gamma = [](float x) -> float {
+            return pow(std::max(std::min(x, 1.f), 0.f), 1.f / 2.2f);
+          };
+          newfb[i].x = uint8_t(255 * gamma(mapped[i].x));
+          newfb[i].y = uint8_t(255 * gamma(mapped[i].y));
+          newfb[i].z = uint8_t(255 * gamma(mapped[i].z));
+          newfb[i].w = uint8_t(255 * std::max(std::min(mapped[i].w, 1.f), 0.f));
+        }
 
-      if (ImGui::Button("Close")) {
-        setShown(false);
-        ImGui::CloseCurrentPopup();
+        // save image
+        std::string fname = "studio.rgb.png";
+        stbi_flip_vertically_on_write(1);
+        int res = stbi_write_png(fname.c_str(), size.x, size.y, 4, (const void *)newfb, 4 * size.x);
+        if (res == 0)
+          std::cerr << "STBI error; could not save image" << std::endl;
+        else
+          std::cout << "Saved to " << fname << std::endl;
       }
-
-      ImGui::EndPopup();
-    }
+      free(newfb);
     }
 
+    if (ImGui::Button("Capture (Depth)")) {
+      auto &fb = context->frame->childAs<sg::FrameBuffer>("framebuffer");
+      auto size = fb.child("size").valueAs<vec2i>();
+      const float *mapped = static_cast<const float *>(fb.map(OSP_FB_DEPTH));
+      size_t npix = size.x * size.y;
+      uint8_t *newfb = (uint8_t *)malloc(npix * sizeof(uint8_t));
+
+      if (newfb) {
+        // scale OSPRay's 0 -> inf depth range to 0 -> 255, ignoring all inf values
+        float minValue = rkcommon::math::inf;
+        float maxValue = rkcommon::math::neg_inf;
+        for (size_t i = 0; i < npix; i++) {
+          if (isinf(mapped[i]))
+            continue;
+          minValue = std::min(minValue, mapped[i]);
+          maxValue = std::max(maxValue, mapped[i]);
+        }
+        std::cout << "min, max: " << minValue << " " << maxValue << std::endl;
+
+        const float rcpRange = 1.f / (maxValue - minValue);
+        for (size_t i = 0; i < npix; i++) {
+          float v = isinf(mapped[i]) ? 1.f : (mapped[i] - minValue) * rcpRange;
+          newfb[i] = uint8_t(v * 255.99f);
+        }
+
+        // save the depth image
+        std::string fname = "studio.depth.png";
+        stbi_flip_vertically_on_write(1);
+        int res = stbi_write_png(fname.c_str(), size.x, size.y, 1, newfb, 1 * size.x);
+        if (res == 0)
+          std::cerr << "STBI error; could not save image" << std::endl;
+        else
+          std::cout << "Saved to " << fname << std::endl;
+      }
+      free(newfb);
+    }
+    
+    ImGui::Separator();
+    if (ImGui::Button("Close")) {
+      setShown(false);
+      ImGui::CloseCurrentPopup();
+    }
+
+    ImGui::EndPopup();
+  }
+  
   }  // namespace voice_plugin
 }  // namespace ospray
