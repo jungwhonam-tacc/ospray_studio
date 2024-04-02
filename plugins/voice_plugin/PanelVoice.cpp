933b8fe5bddeccd6bbbdbd5247022fbe7e29602b
Jung Who Nam <jungwhonam@gmail.com>
2024-04-02 17:14:37 -0500

    Save 3D points in npy file

--- plugins/voice_plugin/PanelVoice.cpp
+++ plugins/voice_plugin/PanelVoice.cpp
@@ -8,6 +8,7 @@
 
 #include "imgui.h"
 #include "stb_image_write.h"
+#include "npy.hpp"
 
 namespace ospray {
   namespace voice_plugin {
@@ -100,6 +101,60 @@ namespace ospray {
           std::cout << "Saved to " << fname << std::endl;
       }
       free(newfb);
+    }
+
+    if (ImGui::Button("Capture (npy)")) {
+      auto &fb = context->frame->childAs<sg::FrameBuffer>("framebuffer");
+      auto size = fb.child("size").valueAs<vec2i>(); // (1024, 768)
+      const float *mapped = static_cast<const float *>(fb.map(OSP_FB_DEPTH));
+      size_t npix = size.x * size.y; // 786432
+      float *xyz = (float *)malloc(npix * 3 * sizeof(float));
+
+      if (xyz) {
+        auto &camera = context->frame->child("camera");
+        auto fovy = camera["fovy"].valueAs<float>(); // 60
+        auto aspect = camera["aspect"].valueAs<float>(); // 1.3333
+
+        // assume the focal length is 1.
+        vec2f imgPlaneSize;
+        imgPlaneSize.y = 2.f * tanf(deg2rad(0.5f * fovy)); // 1.5396
+        imgPlaneSize.x = imgPlaneSize.y * aspect; // 1.1547
+
+        vec3f du_size{imgPlaneSize.x, 0.f, 0.f};
+        vec3f dv_up{0.f, imgPlaneSize.y, 0.f};
+        vec3f du = du_size / size.x;
+        vec3f dv = dv_up / size.y;
+
+        // dir_00 points to the bottom left corner of the image plane
+        vec3f dir_00{0.f, 0.f, -1.f};
+        dir_00 -= 0.5f * du_size;
+        dir_00 -= 0.5f * dv_up;
+
+        int idx = 0;
+        for (int v = size.y - 1; v >= 0; v--) {
+          for (int u = 0; u < size.x; u++) {
+            float depth = mapped[size.x * v + u];
+            vec3f ray_dir = normalize(dir_00 + u * du + v * dv);
+            vec3f pos = ray_dir * depth; // assume the origin is (0,0,0)
+
+            xyz[idx++] = pos.x;
+            xyz[idx++] = pos.y;
+            xyz[idx++] = pos.z;
+          }
+        }
+
+        // save npy file
+        const std::vector<float> data(xyz, xyz + (npix * 3));
+        npy::npy_data_ptr<float> d;
+        d.data_ptr = data.data();
+        d.shape = {npix, 3};
+        d.fortran_order = false; // optional
+        const std::string path{"studio.depth.npy"};
+        npy::write_npy(path, d);
+        std::cout << "Saved to " << path << std::endl;
+      }
+      
+      free(xyz);
     }
     
     ImGui::Separator();
---
+++ plugins/voice_plugin/npy/README.md
@@ -0,0 +1,5 @@
+# npy
+
+`npy.hpp` is copied from [llohse/libnpy](https://github.com/llohse/libnpy), from the v1.0.1 release.
+
+More info [here](https://github.com/llohse/libnpy/releases/tag/v1.0.1)
